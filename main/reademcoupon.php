db9f22c08a806681536b344adce74fdb53c5da48
anshulranjan <40315107+anshulranjan@users.noreply.github.com>
2019-10-11 11:43:31 +0530

    Create reademcoupon.php

---
+++ main/reademcoupon.php
@@ -0,0 +1,27 @@
+<?php
+$con=mysqli_connect("localhost","root","") or die(mysqli_error($con));
+mysqli_select_db($con,"coupon") or die(mysqli_error($con));
+$aa="Reademed";
+session_start();
+$acode=$_SESSION['status'];
+$qq="UPDATE offer SET status='$aa' WHERE ccode='$acode'";
+$rr1=mysqli_query($con,$qq) or die(mysqli_error());
+if($rr1)
+{
+    ?>
+    <script>
+    alert("Coupon Code Successfully Reademed");
+    window.location.href='view.html';
+    </script>
+    <?php
+}
+else
+{
+    ?>
+    <script>
+    alert("Can't Reademed Now. Try again");
+    window.location.href='view.html';
+    </script>
+    <?php
+}
+?>
